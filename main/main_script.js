69865cd54823211df16fb400bf35744f8bd18a24
StefaniaYurzina <94530616+StefaniaYurzina@users.noreply.github.com>
2021-12-07 21:05:19 +0300

    Add files via upload

---
+++ main/main_script.js
@@ -0,0 +1,19 @@
+function kamch() {
+    window.location.href = '../kamch/kamchatka.html';
+};
+
+function ice() {
+    window.location.href = '../ice/ice.html';
+};
+
+function boost() {
+    window.location.href = '../boost/boost.html';
+};
+
+function gallery() {
+    window.location.href = '../gallery/gallery.html';
+};
+
+function behance() {
+    window.location.href = 'https://www.behance.net/stefania';
+};
